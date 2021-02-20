f778ae783b034fd4bf1b0e1b0c8d0bd4ed7eefde
Esdran26 <vargasandres2627@gmail.com>
2021-02-20 14:59:24 -0500

    Login Auth

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,29 +1,129 @@
 import Head from 'next/head';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 const Login = () => {
-  const handleLogin = e => {
-    e.preventDefault();
-  }
+  const { register, handleSubmit, errors } = useForm();
+  const [serverError, setServerError] = useState('');
+
+  const onSubmit = data => {
+    axios.post(
+      'https://portfolio-backend-mern.herokuapp.com/api/auth/', 
+      data
+    )
+    .then(res => {
+      console.log(res);
+    })
+    .catch(error => {
+      setServerError(error.response.data.msg);
+    })
+  };
 
   return(
     <>
       <Head>
         <title>Login</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"></link>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossOrigin="anonymous"></link>
       </Head>
 
       <div className="container pt-5">
-        <form onSubmit={ handleLogin }>
+
+        {
+          serverError !== '' &&
+          <div className="alert alert-danger">
+            { serverError }
+          </div>
+        }
+
+        <form onSubmit={ handleSubmit(onSubmit) }>
           <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            <label 
+              htmlFor="exampleInputEmail1" 
+              className="form-label"
+            >
+              Email address
+            </label>
+
+            <input 
+              type="email" 
+              className="form-control" 
+              id="exampleInputEmail1" 
+              aria-describedby="emailHelp"
+              name="email"
+              ref={ register({ 
+                required: true, 
+                pattern: /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/ 
+              })}
+              placeholder="example@email.com"
+            />
+            
+
+            {
+              errors.email &&
+                <div>
+                  <br/>
+                  <div 
+                    id="emailHelp" 
+                    className="alert alert-danger" role="alert"
+                  >
+                    
+                    Please enter a valid email
+                  </div>
+                </div>
+            }
+
+            {
+              !errors.email &&
+                <div 
+                id="emailHelp" 
+                className="form-text" role="alert"
+                >
+                  We'll never share your email with anyone else.
+                </div>
+            }
           </div>
           <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" />
+            <label 
+              htmlFor="exampleInputPassword1" 
+              className="form-label"
+            >
+              Password
+            </label>
+
+            <input 
+              type="password" 
+              className="form-control" 
+              id="exampleInputPassword1" 
+              name="password"
+              ref={ register({
+                required: true,
+                minLength: 6
+              })}
+              placeholder="Minimum 6 characters"
+            />
+
+            {
+              errors.password &&
+                <div>
+                  <br/>
+                  <div 
+                    id="passwordHelp" 
+                    className="alert alert-danger" role="alert"
+                  >
+                    
+                    Please enter a valid password
+                  </div>
+                </div>
+            }
+
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button 
+            type="submit" 
+            className="btn btn-primary"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </>
