599ed70ede76c0ca636b2e70478a2d361b1e901c
Esdran26 <vargasandres2627@gmail.com>
2021-02-25 14:00:27 -0500

    Fixed #5

--- pages/timeline.jsx
+++ pages/timeline.jsx
@@ -25,6 +25,16 @@ const Timeline = () => {
         <meta property="twitter:title" content="Andres Vargas | Portfolio" />
         <meta property="twitter:description" content="This is my personal website, when I show my projects, timeline, who am I and more." />
         <meta property="twitter:image" content="https://res.cloudinary.com/esdran26/image/upload/v1614268406/Mockup-Portfolio_q00ya8.png" />
+
+
+        <meta http-equiv="Content-Type" content="text/html; ISO-8859-1" />
+        <meta name="DC.Language" scheme="RFC1766" content="English" />
+        <meta name="AUTHOR" content="Andres Felipe Vargas Gomez" />
+        <meta name="KEYWORDS" content="Portfolio,Website,Personal,FrontEnd Developer,Web Developer" />
+        <meta name="Resource-type" content="Document" />
+        <meta name="DateCreated" content="Thu, 25 February 2021 11:30:00 GMT+5" />
+        <meta name="Revisit-after" content="3 days" />
+        <meta name="robots" content="ALL" />
       </Head>
 
       <MobileNavbar section="Timeline" />
