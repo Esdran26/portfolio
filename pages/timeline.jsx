11035bbca4d3f0c7abb635eae29e0c4f70af1642
Esdran26 <vargasandres2627@gmail.com>
2021-02-25 13:22:35 -0500

    Fixed #1

--- pages/timeline.jsx
+++ pages/timeline.jsx
@@ -15,8 +15,8 @@ const Timeline = () => {
         <link rel="shortcut icon" href="https://res.cloudinary.com/esdran26/image/upload/v1614266954/logo_qwetej.ico" type="image/x-icon"/>
         <meta name="REPLY-TO" content="vargasandres2627@gmail.com" />
         <link rev="made" href="mailto:vargasandres2627@gmail.com" />
-        <meta property="og:image" content="https://res.cloudinary.com/esdran26/image/upload/v1614268406/Mockup-Portfolio_q00ya8.png"></meta>
         <meta name="DESCRIPTION" content="This is my personal website, when I show my projects, timeline, who am I and more." />
+        <meta name="IMAGE" content="https://res.cloudinary.com/esdran26/image/upload/v1614268406/Mockup-Portfolio_q00ya8.png"></meta>
         <meta name="KEYWORDS" content="Portfolio,Website,Personal,FrontEnd Developer,Web Developer" />
         <meta name="Resource-type" content="Document" />
         <meta name="DateCreated" content="Thu, 25 February 2021 11:30:00 GMT+5" />
